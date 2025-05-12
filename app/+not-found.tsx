dc979b723ce90650c0744ba08cf88b8030ac2e21
nirrie <97035721@st.deltion.nl>
2025-05-12 22:18:55 +0200

    update +not found screen with same layout as index

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -6,15 +6,14 @@ export default function NotFoundScreen() {
     return (
         <>
             <Stack.Screen options={{ title: 'Not found' }} />
-            <View style={styles.container}>
                 <LinearGradient
-                    colors={['rgba(244, 109, 221, 0.8)', 'transparent']}
-                    style={styles.background}
-                />
+                    colors={['#B88566', '#FAC3A5', '#FBEDE0']}
+                    style={styles.container}
+            >
                 <Link href="/" style={styles.link}>
-                    Go to home screen!
-                </Link>
-            </View>
+                Home
+                 </Link>
+            </LinearGradient>
         </>
     );
 }
@@ -23,18 +22,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        backgroundColor: 'orange',
         justifyContent: 'center',
     },
-    background: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        height: 300,
-    },
     link: {
-        fontsize: 20,
+        fontSize: 20,
         textDecorationLine: 'underline',
         color: '#fff',
     },
