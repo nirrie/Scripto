f6ba906f37118f1ef65c7223fcaa93b8529ce5c1
nirrie <97035721@st.deltion.nl>
2025-05-21 10:23:18 +0200

    add splash screen animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { Animated, View, Image, StyleSheet } from "react-native";
 import { Stack } from "expo-router";
 import { useFonts } from 'expo-font';
 
@@ -5,10 +7,43 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     Roboto: require('../assets/fonts/Roboto-Italic-VariableFont_wdth,wght.ttf'),
   });
+  const [fadeAnim] = useState(new Animated.Value(1));
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 1000,
+      delay: 1000,
+      useNativeDriver: true,
+    }).start();
+    }, []);
   return (
+    <>
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
-    </Stack>
+        </Stack>
+       <Animated.View style={[StyleSheet.absoluteFill, { opacity: fadeAnim}]}>
+        <View style={styles.splashScreen}>
+          <Image 
+          source={require('../assets/images/splash-icon.png')}
+          style={styles.splashIcon}
+          />
+        </View>
+      </Animated.View>
+      </>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  splashScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#FBEDE0",
+  },
+  splashIcon: {
+    width: 200,
+    height: 200,
+  },
+});
