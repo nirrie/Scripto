0fc9edb41e4b59eac1b89c1b87c0363c17bd1e30
nirrie <97035721@st.deltion.nl>
2025-05-23 11:27:23 +0200

    add AsyncStorage package

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
-import {Modal, Text, View, TouchableWithoutFeedback, StyleSheet } from "react-native";
+import {
+  Modal,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  StyleSheet,
+} from "react-native";
 import { Link } from "expo-router";
 import { useRouter } from "expo-router";
 import { useState } from "react";
@@ -7,25 +13,22 @@ import { LinearGradient } from "expo-linear-gradient";
 import Button from "@/.expo/components/Button";
 import Note from "@/.expo/components/Note";
 
-
-
 export default function Index() {
   const router = useRouter();
   const [ShowNote, setShowNote] = useState(false);
-  
+
   return (
     <LinearGradient
-      colors={['#B88566', '#FAC3A5' , '#FBEDE0']}
-        locations={[0, 0.5, 1]}
-        style={styles.container}
-      >
-        <Text style={styles.title}>Scripto</Text>
-      
-      
-      <Link href="/saved" style={styles.button}>
-      </Link>
+      colors={["#B88566", "#FAC3A5", "#FBEDE0"]}
+      locations={[0, 0.5, 1]}
+      style={styles.container}
+    >
+      <Text style={styles.title}>Scripto</Text>
+
+      <Link href="/saved" style={styles.button}></Link>
       <Button
-        icon="add-circle-outline" style={styles.footerContainer}
+        icon="add-circle-outline"
+        style={styles.footerContainer}
         onPress={() => setShowNote(true)}
       />
       <Modal visible={ShowNote} animationType="slide" transparent={true}>
@@ -39,56 +42,53 @@ export default function Index() {
             <TouchableWithoutFeedback onPress={() => {}}>
               <View style={styles.noteWrapper}>
                 <Note onClose={() => setShowNote(false)} />
-                </View>
-          </TouchableWithoutFeedback>
+              </View>
+            </TouchableWithoutFeedback>
           </View>
         </TouchableWithoutFeedback>
-
-
       </Modal>
-              </LinearGradient>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 50,
-    fontFamily: 'Roboto',
-    color: '#B88566',
-    fontWeight: 'bold',
+    fontFamily: "Roboto",
+    color: "#B88566",
+    fontWeight: "bold",
     marginTop: 60,
     marginBottom: 20,
   },
   button: {
     padding: 20,
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 5,
     marginTop: 20,
   },
   modalContainer: {
     flex: 1,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   noteWrapper: {
-    width: '90%',
-    height: '80%',
-    backgroundColor: '#fbede0',
+    width: "90%",
+    height: "80%",
+    backgroundColor: "#fbede0",
     borderRadius: 12,
     padding: 20,
   },
   footerContainer: {
     flex: 1 / 3,
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
-
