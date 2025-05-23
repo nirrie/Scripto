af7b37060c54a30dff244f22715ba63d7c579400
nirrie <97035721@st.deltion.nl>
2025-05-23 11:57:03 +0200

    add axios setup, add token to every request

---
+++ lib/api.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api',
+});
+
+api.interceptors.request.use(async (config) => {
+    const token = await AsyncStorage.getItem('auth_token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
