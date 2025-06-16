70362e329b0bc03b26b9386befe787c3064beea5
Nirmin <97035721@st.deltion.nl>
2025-06-16 10:48:59 +0200

    Update register modal to match app layout. Add correct error response in catch block

--- components/RegisterModal.tsx
+++ components/RegisterModal.tsx
@@ -35,12 +35,13 @@ export default function RegisterModal({ visible, onClose, onRegisterSuccess, onS
             onRegisterSuccess(token);
             onClose();
         } catch (error: any) {
-              console.log(error);
-                Alert.alert("Registration failed");
-            
-            console.log(error);
+            if (error.response && error.response.data === 422) {
+                const messages = error.response.data.errors;
+                console.log("Validation Errors:", messages);
+            }
         }
-    }
+            Alert.alert("Registration succesfull!");
+        }
     
     return (
             <Modal visible={visible} animationType="slide" transparent={true}>
@@ -52,7 +53,7 @@ export default function RegisterModal({ visible, onClose, onRegisterSuccess, onS
                             Login
                         </Text>
                     </Text>
-                    <Text style={styles.title}>Login</Text>
+                    <Text style={styles.title}>Create a new account</Text>
                     <TextInput
                         placeholder="Name"
                         value={name}
@@ -93,52 +94,71 @@ export default function RegisterModal({ visible, onClose, onRegisterSuccess, onS
             
     }
 
-const styles = StyleSheet.create({
-    overlay: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-    },
-    modalContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    title: {
-        fontSize: 24,
-    },
-    input: {
-        width: "80%",
-        padding: 10,
-        borderWidth: 1,
-        borderColor: "#ccc",
-        borderRadius: 5,
-        marginBottom: 20,
-    },
-    button: {
-        backgroundColor: "#007BFF",
-        padding: 10,
-        borderRadius: 5,
-        marginBottom: 10,
-    },
-    buttonText: {
-        color: "#fff",
-        fontWeight: "bold",
-    },
-    cancel: {
-        backgroundColor: "#FF0000",
-    },
-    cancelText: {
-        color: "#fff",
-    },
-    linkText: {
-        color: "#007BFF",
-        textDecorationLine: "underline",
-        marginBottom: 20,
-    },
-    link: {
-        color: "#007BFF",
-        textDecorationLine: "underline",
-    },
-});
+    const styles = StyleSheet.create({
+        overlay: {
+            flex: 1,
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        modalContainer: {
+            width: "85%",
+            backgroundColor: "#fac3a5",
+            borderRadius: 20,
+            padding: 20,
+            elevation: 5,
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        title: {
+            fontSize: 24,
+            fontFamily: "Roboto, sans-serif",
+            fontWeight: "bold",
+            marginBottom: 20,
+            color: "#B88566",
+        },
+        input: {
+            width: "100%",
+            padding: 10,
+            borderWidth: 1,
+            borderColor: "#B88566",
+            opacity: 0.5,
+            backgroundColor: "#fff",
+            borderRadius: 5,
+            marginBottom: 20,
+        },
+        button: {
+            backgroundColor: "#e48368",
+            paddingVertical: 10,
+            paddingHorizontal: 30,
+            borderRadius: 5,
+            marginBottom: 10,
+        },
+        buttonText: {
+            color: "#fff",
+            fontWeight: "bold",
+        },
+        cancel: {
+            fontWeight: "bold",
+            backgroundColor: "#FF0000",
+        },
+        cancelText: {
+            backgroundColor: "#fac3a5",
+        },
+        linkText: {
+            marginTop: 20,
+            color: "#b88566",
+        },
+        link: {
+            color: "#007BFF",
+            textDecorationLine: "underline",
+        },
+    
+    });
