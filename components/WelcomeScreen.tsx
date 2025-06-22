99aabd67442f5998f8b8d50043e41558668bd31a
Nirmin <97035721@st.deltion.nl>
2025-06-22 19:05:36 +0200

    Add background image to welcome screen.

--- components/WelcomeScreen.tsx
+++ components/WelcomeScreen.tsx
@@ -1,56 +1,96 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Pressable,
+    Image
+} from 'react-native';
 
-export default function WelcomeScreen({ onLogin, onRegister }: {
+export default function WelcomeScreen({ onLogin, onRegister, }: {
     onLogin: () => void;
     onRegister: () => void;
-}) {
+    
+})
+
+{
 
   return (
-    <View style={styles.container}>
+      <View style={styles.container}>
+          <Image source={require('../assets/images/splash.png')} style={styles.image} />
       <Text style={styles.title}>Welcome to Scripto!</Text>
           <Text style={styles.description}>Scripto is a simple note app where your notes are safely stored in your personal account.
               It is designed to be easy to use, with not to many features, so you can focus on writing your notes!
-      </Text>
+          </Text>
+          <View style={styles.buttonContainer}>
           <Pressable style={styles.button} onPress={onLogin}>
         <Text style={styles.buttonText}>Login</Text>
           </Pressable>
           <Pressable style={styles.button} onPress={() => {console.log("Register pressed"); onRegister();}}>
               <Text style={styles.buttonText}>Register</Text>
               </Pressable>
+              </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        ...StyleSheet.absoluteFillObject,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#FBEDE0',
         padding: 20,
     },
+    image: {
+        width: 150,
+        height: 150,
+        borderRadius: 20,
+        marginBottom: 20,
+        resizeMode: "contain",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 5,
+    },
     title: {
+        fontFamily: "Roboto",
         fontSize: 32,
         fontWeight: 'bold',
         marginBottom: 20,
         color: '#B88566',
     },
     description: {
+        fontFamily: "Roboto",
         fontSize: 16,
         textAlign: 'center',
         marginBottom: 40,
         color: '#333',
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        gap: 10,
+        marginTop: 20,
+        
+    },
     button: {
+        flex: 1,
         backgroundColor: '#B88566',
-        paddingVertical: 15,
-        paddingHorizontal: 30,
-        borderRadius: 5,
-        marginVertical: 10,
+        paddingVertical: 12,
+        borderRadius: 10,
+        marginHorizontal: 5,
+        elevation: 3,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
     },
     buttonText: {
         color: '#FFF',
-        fontSize: 18,
+        fontFamily: "Roboto",
+        fontSize: 16,
+        fontWeight: '600',
+        textAlign: 'center',
     },
     });
 
